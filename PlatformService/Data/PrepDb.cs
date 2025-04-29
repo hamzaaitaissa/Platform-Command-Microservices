6823c190ce7911970f6e55b789496a7a9189a901
Hamza Ait Aissa <hamzaaitaissa8@gmail.com>
2025-04-29 22:47:57 +0100

    Dev: PrepDb Created and registred

---
+++ PlatformService/Data/PrepDb.cs
@@ -0,0 +1,31 @@
+﻿namespace PlatformService.Data
+{
+    //we dont need to create any instance of it
+    public static class PrepDb
+    {
+        public static void PrePopulation(IApplicationBuilder app)
+        {
+            using(var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>()); 
+            }
+        }
+
+        private static void SeedData(AppDbContext context)
+        {
+            if (!context.Platforms.Any())
+            {
+                Console.WriteLine("Seeding data...");
+
+                context.Platforms.AddRange(
+                    new Models.Platform() { Name = "Dot net", Publisher = "Microsoft", Cost = "Free"},
+                    new Models.Platform() { Name = "SQL Server Express", Publisher = "Microsoft", Cost = "Free"},
+                    new Models.Platform() { Name = "Kubernetes", Publisher = "Cloud Native Computing Foundation", Cost = "Free"}
+                    );
+            }
+            else{
+                Console.WriteLine("we already have data");
+            }
+        }
+    }
+}
--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -27,4 +27,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+PrepDb.PrePopulation(app);
+
 app.Run();
