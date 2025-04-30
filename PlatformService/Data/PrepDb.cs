db5ea9d6a01a1ee653aa60e105b677f8ea89e1ff
Hamza Ait Aissa <hamzaaitaissa8@gmail.com>
2025-04-30 19:23:35 +0100

    Dev: Creating DTOs

--- PlatformService/Data/PrepDb.cs
+++ PlatformService/Data/PrepDb.cs
@@ -1,6 +1,9 @@
-﻿namespace PlatformService.Data
+﻿using System.Numerics;
+
+namespace PlatformService.Data
 {
     //we dont need to create any instance of it
+    //If the database is empty, let’s fill in some cool starter data so users aren’t staring at a blank page.
     public static class PrepDb
     {
         public static void PrePopulation(IApplicationBuilder app)
@@ -22,6 +25,8 @@
                     new Models.Platform() { Name = "SQL Server Express", Publisher = "Microsoft", Cost = "Free"},
                     new Models.Platform() { Name = "Kubernetes", Publisher = "Cloud Native Computing Foundation", Cost = "Free"}
                     );
+                //unless youy save it, it aint going anywhere
+                context.SaveChanges();
             }
             else{
                 Console.WriteLine("we already have data");
