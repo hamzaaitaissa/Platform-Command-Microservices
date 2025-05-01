f01421aabf670d56fa15e87851ddb88f06b9a631
Hamza Ait Aissa <hamzaaitaissa8@gmail.com>
2025-05-01 20:29:11 +0100

    Dev: configuring AutoMapper

---
+++ PlatformService/Profiles/PlatformProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using PlatformService.DTOs;
+using PlatformService.Models;
+
+namespace PlatformService.Profiles
+{
+    public class PlatformProfile : Profile
+    {
+        public PlatformProfile()
+        {
+            // Source(Model) -> Target(Dto)
+            CreateMap<Platform, PlatformReadDto>();
+        }
+    }
+}
